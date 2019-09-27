0db94802e1fe358ea18d95a7dcf1060f07c55a11
Vladislav Maksimovic <daksrapsody@gmail.com>
2019-09-27 03:05:51 +0200

    background

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,13 +7,20 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- BOOTSTRAP -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      
+        <!--CUSTOM CSS -->
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
         <title> Kulminacija znanja </title>
         
     </head>
     <body>
         <!-- gde god stavimo da smo extend ovaj fajl i napravimo sekciju kod,taj citav kod ce biti
         prebacen umesto ovog ovde yield-a  -->
-
         @yield('kod')
+
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
 </html>
